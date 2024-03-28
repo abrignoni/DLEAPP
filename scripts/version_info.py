4f7b9282775098326bb67a03e09d4591d96143e8
Johann POLEWCZYK <johannplw@me.com>
2024-03-28 14:23:26 +0100

    New Tkinter GUI

--- scripts/version_info.py
+++ scripts/version_info.py
@@ -1,4 +1,4 @@
-dleapp_version = '1.0'
+dleapp_version = '1.1'
 
 # Contributors List
 # Format = [ Name, Blog-url, Twitter-handle, Github-url]
@@ -8,5 +8,5 @@ dleapp_contributors = [
     [ 'Yogesh Khatri', 'https://swiftforensics.com', '@SwiftForensics', 'https://github.com/ydkhatri'],
     [ 'Geraldine Blay', 'https://gforce4n6.blogspot.com', '@i_am_the_gia', 'https://github.com/gforce4n6'],
     [ 'Kevin Pagano', 'https://stark4n6.com', '@KevinPagano3', 'https://github.com/stark4n6'],
-
+    [ 'Johann Polewczyk', 'https://www.linkedin.com/in/johann-polewczyk-6a905425/', '@johannplw', 'https://github.com/Johann-PLW'],
 ]
